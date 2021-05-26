4fbe8116ca0d6fba755efd025500ade3f7210660
Muhammad Saad Shafiq <saadshafiq259@gmail.com>
2021-05-26 17:39:38 +0500

    SupplyChain Mobile initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('Hello World!'),
+    );
+  }
+}
